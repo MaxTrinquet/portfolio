7fba42aed2e9c6a0ed6a7f9c21c4232b8324c5a5
Maxime Trinquet <maxime.trinquet@gmail.com>
2021-05-17 15:05:32 +0200

    Copy and paste email adress fixed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container } from 'react-bootstrap';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import PortfolioContext from '../../context/context';
 import Title from '../Title/Title';
 
@@ -8,23 +9,24 @@ const Contact = () => {
   const { contact } = useContext(PortfolioContext);
   const { cta, btn, email } = contact;
 
+  const [copied, setCopied] = useState(false);
+
+  const copyAlert = () => {
+    setCopied(false);
+    return <p>Successfully copied to clipboard ! </p>;
+  };
+
   return (
     <section id="contact">
       <Container>
         <Title title="Contact" />
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
-            <p className="contact-wrapper__text">
-              {cta || 'Would you like to work with me? Awesome!'}
-            </p>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
-            >
-              {btn || "Let's Talk"}
-            </a>
+            <p className="contact-wrapper__text">{cta}</p>
+            <CopyToClipboard text={email} onCopy={() => setCopied(true)}>
+              <p className="contact-wrapper__text cta-btn cta-btn--resume">{btn}</p>
+            </CopyToClipboard>
+            <p className="text-clipboard">{copied ? copyAlert() : null}</p>
           </div>
         </Fade>
       </Container>
